0f7532d521233b69787be4c47f04bf74b4b244c8
jarvis-exes <8696771553shubham@gmail.com>
2023-10-02 15:15:14 +0530

    Added Events

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,51 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            title : 'Phone',
+            price : 999,
+            qty : 1,
+            img : '' 
+        }
+    }
+
+    increaseQuantity = ()=>{
+        this.state.qty++;
+        console.log(this.state.qty);
+    }
+
     render(){
+        const {title, price, qty} = this.state;
         return (
             <div className='cart-item'>
                 <div className='.left-block'>
                     <img style={ Style.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }>price</div>
-                    <div style={ {color:'#777'} }>Rs 999</div>
-                    <div style={ {color:'#777'} }>Oty :1</div>
+                    <div style={ {fontSize: 25} }>{title}</div>
+                    <div style={ {color:'#777'} }>Rs. {price}</div>
+                    <div style={ {color:'#777'} }>Quantity : {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/3416/3416075.png"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/181/181667.png"
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/10147/10147931.png "
+                        />
                     </div>
                 </div>
             </div>
